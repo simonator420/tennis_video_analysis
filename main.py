ac087fefd61acd7000f2f544cb1301c6636de6ab
simonator420 <salajsimon@gmail.com>
2025-06-30 00:22:31 +0200

    Smoother ball detection

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils import (read_video,
 
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+import cv2
 
 def main():
     # Read Video
@@ -13,11 +14,13 @@ def main():
     player_tracker = PlayerTracker(model_path='yolov8x')
     ball_tracker = BallTracker(model_path='models/yolo5_last.pt')
     player_detections = player_tracker.detect_frames(video_frames,
-                                                     read_from_stub=False,
+                                                     read_from_stub=True,
                                                      stub_path="tracker_stubs/player_detections.pkl")
     ball_detections = ball_tracker.detect_frames(video_frames,
-                                                 read_from_stub=False,
+                                                 read_from_stub=True,
                                                  stub_path="tracker_stubs/ball_detections.pkl")
+    
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
     
     # Court Line Detections
     court_model_path = "models/keypoints_model.pth"
@@ -30,6 +33,10 @@ def main():
     
     # Draw court Keypoints
     output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
+    
+    # Draw frame number
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}",(10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
     save_video(output_video_frames, "output_videos/output_video.avi", fps)
     
--- trackers/ball_tracker.py
+++ trackers/ball_tracker.py
@@ -7,6 +7,19 @@ class BallTracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
         
+    def interpolate_ball_positions(self, ball_positions):
+        ball_positions = [x.get(1,[]) for x in ball_positions]
+        
+        df_ball_positions = pd.DataFrame(ball_positions,columns=['x1','y1','x2','y2'])
+
+        # Interpolate the missing values
+        df_ball_positions = df_ball_positions.interpolate()
+        df_ball_positions = df_ball_positions.bfill()
+
+        ball_positions = [{1:x} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
+        
     def detect_frames(self, frames, read_from_stub=False, stub_path=None):
         ball_detections = []
         
